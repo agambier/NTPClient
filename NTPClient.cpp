1163f5f3abd39d1552ac0ea54a4a4c814e5fe780
Alexandre Gambier <agambier.dev@gmail.com>
2019-09-16 10:43:52 +0200

    Simplify getFormattedDate()

--- NTPClient.cpp
+++ NTPClient.cpp
@@ -210,28 +210,12 @@ String NTPClient::getFormattedTime(unsigned long secs) {
 // Based on https://github.com/PaulStoffregen/Time/blob/master/Time.cpp
 // currently assumes UTC timezone, instead of using this->_timeOffset
 String NTPClient::getFormattedDate(unsigned long secs) {
-  unsigned long rawTime = (secs ? secs : this->getEpochTime()) / 86400L;  // in days
-  unsigned long days = 0, year = 1970;
-  uint8_t month;
-  static const uint8_t monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31};
-
-  while((days += (LEAP_YEAR(year) ? 366 : 365)) <= rawTime)
-    year++;
-  rawTime -= days - (LEAP_YEAR(year) ? 366 : 365); // now it is days in this year, starting at 0
-  days=0;
-  for (month=0; month<12; month++) {
-    uint8_t monthLength;
-    if (month==1) { // february
-      monthLength = LEAP_YEAR(year) ? 29 : 28;
-    } else {
-      monthLength = monthDays[month];
-    }
-    if (rawTime < monthLength) break;
-    rawTime -= monthLength;
-  }
-  String monthStr = ++month < 10 ? "0" + String(month) : String(month); // jan is month 1  
-  String dayStr = ++rawTime < 10 ? "0" + String(rawTime) : String(rawTime); // day of month  
-  return String(year) + "-" + monthStr + "-" + dayStr + "T" + this->getFormattedTime(secs ? secs : 0) + "Z";
+	uint16_t year;
+	uint8_t month, day;
+	getDate(&year, &month, &day, secs );
+	String monthStr = ( month < 10 ) ? "0" + String( month ) : String( month ); // jan is month 1  
+	String dayStr = ( day < 10 ) ? "0" + String( day ) : String( day ); // day of month  
+	return String( year ) + "-" + monthStr + "-" + dayStr + "T" + this->getFormattedTime( secs ) + "Z";
 }
 
 void NTPClient::end() {
